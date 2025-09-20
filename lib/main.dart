17945b601b2e231d931e121c90e663909a8537c5
Gabs-489 <g.zambranoz@uniandes.edu.co>
2025-09-19 23:50:21 -0500

    Home Correction

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
 
   final List<Widget> _pages = const [
-    HomePage(),
+    Home(),
     NotificationsPage(),
     DetailEvent(),
     ProfilePage(), 
