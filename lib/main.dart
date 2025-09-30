5a9513e9d05b238a90d1d32a9ac8b30d94f6083b
Gabs-489 <g.zambranoz@uniandes.edu.co>
2025-09-30 16:53:19 -0500

    This changes had to be made on the proyect to connect it to the firebase proyect

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:app_flutter/firebase_options.dart';
 import 'package:app_flutter/pages/listEvents.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'pages/notification.dart';
 import 'pages/home.dart';
@@ -8,7 +10,11 @@ import 'pages/carga.dart';
 import 'pages/inicio.dart';
 import 'pages/login.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
