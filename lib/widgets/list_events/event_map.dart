3c3a93a12734ef57d79a3a1376066d544cfb4eab
aarevalom0 <a.arevalom@uniandes.edu.co>
2025-10-03 12:00:12 -0500

    Upload first version to obtain user location

--- lib/widgets/list_events/event_map.dart
+++ lib/widgets/list_events/event_map.dart
@@ -1,6 +1,7 @@
 import 'package:app_flutter/pages/events/model/event.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
 class EventsMapView extends StatefulWidget {
   final List<Event> events;
@@ -14,11 +15,16 @@ class EventsMapView extends StatefulWidget {
 class _EventsMapViewState extends State<EventsMapView> {
   GoogleMapController? _mapController;
   Set<Marker> _markers = {};
+  // USER LOCATION
+  LatLng? _userPosition;
+
 
   @override
   void initState() {
     super.initState();
+    _determinePosition();
     _createMarkers();
+    
   }
 
   @override
@@ -56,18 +62,69 @@ class _EventsMapViewState extends State<EventsMapView> {
       );
     }).whereType<Marker>().toSet();
 
+    if (_userPosition != null) {
+      _markers.add(
+        Marker(
+          markerId: const MarkerId("user_location"),
+          position: _userPosition!,
+          infoWindow: const InfoWindow(title: "You are here"),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueAzure),
+        ),
+      );
+    }
+
     if (mounted) setState(() {});
+  }
+
+
+  Future<void> _determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    // GPS active
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) return;
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) return;
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return;
+    }
+
+    // Get current location
+    final position = await Geolocator.getCurrentPosition().catchError((e) {
+  print("Error al obtener ubicación: $e");
+  return null;
+});
+
+if (position != null) {
+  setState(() {
+    _userPosition = LatLng(position.latitude, position.longitude);
+    _createMarkers();
+  });
+} else {
+  print("No se pudo obtener ubicación, usando fallback Bogotá.");
+}
+    setState(() {
+      _userPosition = LatLng(position.latitude, position.longitude);
+      _createMarkers();
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     // Calcular centro del mapa
-    final LatLng center = widget.events.isNotEmpty
-        ? LatLng(
-            widget.events.first.location.coordinates[0],
-            widget.events.first.location.coordinates[1],
-          )
-        : const LatLng(4.7110, -74.0721); // Bogotá por defecto
+    final LatLng center = _userPosition ??
+        (widget.events.isNotEmpty
+            ? LatLng(
+                widget.events.first.location.coordinates[0],
+                widget.events.first.location.coordinates[1],
+              )
+            : const LatLng(4.7110, -74.0721)); // Bogotá por defecto
 
     return GoogleMap(
       initialCameraPosition: CameraPosition(
